6a406069f96693ccdc463663da2b86329b3e649a
gabriel <gabrielcmoris@gmail.com>
2022-09-30 13:28:45 +0200

    SOLID design principles. 1- single responsability

---
+++ SOLID-design-principles/single-responsibility-principle.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+class Journal {
+  constructor() {
+    this.entries = {};
+  }
+
+  addEntry(text) {
+    let c = ++Journal.count;
+    let entry = `${c}: ${text}`;
+    this.entries[c] = entry;
+    return c;
+  }
+
+  removeEntry(index) {
+    delete this.entries[index];
+  }
+
+  toString() {
+    return Object.values(this.entries).join("\n");
+  }
+  // This principle says that It is better that this object has only one responsability, and then we would do
+  // the rest of tasks in other objects
+  //   save(filename) {
+  //     fs.writeFileSync(filename, this.toString());
+  //   }
+
+  //   load(filename) {
+  //     //
+  //   }
+
+  //   loadFromUrl(url) {
+  //     //
+  //   }
+}
+Journal.count = 0;
+
+class PersistenceManager {
+  preprocess(j) {
+    //
+  }
+
+  saveToFile(journal, filename) {
+    fs.writeFileSync(filename, journal.toString());
+  }
+}
+
+let j = new Journal();
+j.addEntry("I cried today.");
+j.addEntry("I ate a bug.");
+console.log(j.toString());
+
+let p = new PersistenceManager();
+let filename =
+  "/home/gabriel/udemy/design-patterns/SOLID-design-principles/journal.txt";
+p.saveToFile(j, filename);
+
+// separation of concerns
