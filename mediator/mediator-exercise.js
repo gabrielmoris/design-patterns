2b8cec8f6d4ed6831d800a6c22ba3c79ecbb6e18
gabriel <gabrielcmoris@gmail.com>
2024-04-19 03:59:01 -0400

    mediator exercise

--- mediator/mediator-exercise.js
+++ mediator/mediator-exercise.js
@@ -1,18 +1,49 @@
-class Mediator
-{
-  // todo
-}
-
-class Participant
-{
-  constructor(mediator)
-  {
-    // todo
+class Event {
+  constructor() {
+    this.handlers = new Map();
+    this.count = 0;
   }
 
-  say(n)
-  {
-    // todo
+  subscribe(handler) {
+    this.handlers.set(this.count++, handler);
+    return this.count;
+  }
+
+  unsubscribe(index) {
+    this.handlers.delete(index);
+  }
+
+  fire(sender, args) {
+    this.handlers.forEach((v, k) => {
+      v(sender, args);
+    });
+  }
+}
+
+class Mediator {
+  constructor() {
+    this.alert = new Event();
+  }
+
+  broadcast(sender, message) {
+    this.alert.fire(sender, message);
+  }
+}
+let participantID = 0;
+class Participant {
+  constructor(mediator, id = participantID) {
+    this.value = 0;
+    this.id = id;
+    this.mediator = mediator;
+    mediator.alert.subscribe(this.alert.bind(this)); // the alert need to have the enviroment from here
+  }
+
+  alert(sender, n) {
+    if (sender !== this) this.value += n;
+  }
+
+  say(n) {
+    this.mediator.broadcast(this, n);
   }
 }
 
